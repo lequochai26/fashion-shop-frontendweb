701d7423bdfa5746b342654e848be436ec71abb5
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-17 04:05:32 +0700

    Added APIFetcher.

---
+++ src/utils/APIFetcher.ts
@@ -0,0 +1,40 @@
+export const API_URL: string = "http://localhost:3000";
+
+export async function apiFetch({ path, method, body, onSuccess, onFailed }: MakeRequestParam) {
+    try {
+        var response = await fetch(
+            `${API_URL}${path}`,
+            {
+                method,
+                body: body && (
+                    body instanceof FormData
+                    ? body
+                    : JSON.stringify(body)
+                ),
+                mode: 'cors',
+                credentials: 'include',
+                headers: [
+                    [ "Content-Type", (
+                        body instanceof FormData
+                        ? "multipart/form-data"
+                        : "application/json"
+                    ) ]
+                ]
+            }
+        );
+    }
+    catch (error: any) {
+        onFailed(error);
+        return;
+    }
+
+    onSuccess(response);
+}
+
+export interface MakeRequestParam {
+    path: string,
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    body?: any | FormData | undefined,
+    onSuccess: (response: Response) => Promise<void>,
+    onFailed: (error: any) => Promise<void>
+}
