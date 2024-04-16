7ec732a97edc459b4a362377fb47a213c21d1e29
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-17 02:00:43 +0700

    Installed Tailwind CSS framework.

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust paths as needed
+    theme: {
+      extend: {}, // Add custom Tailwind CSS classes here
+    },
+    plugins: [], // Add any Tailwind plugins if needed
+  };
