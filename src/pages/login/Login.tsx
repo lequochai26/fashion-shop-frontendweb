e14c7d81c53bb48c84a84a79bd8df590a8376604
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-18 01:50:13 +0700

    change css

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,11 +8,10 @@ export default function Login(){
         <img
             alt="Fashion Shop's Logo"
             src="/logo.png"
-            className="w-48 h-48 mb-5 ml-80 mx-auto "
+            className="w-48 h-48 mb-5 ml-6 mr-auto "
         />
         <div className=''>
         <input
-            // className="w-48 h-8 mb-4  border-gray-300  pl-5"
             className='shadow appearance-none border rounded w-200 h-30 py-2 px-3 text-gray-700 mb-4 mx-auto mr-52'
             type="email"
         /><br/>
