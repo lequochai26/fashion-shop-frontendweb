05718ca14b540a114b244c0064716bb7e6d4996d
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-17 15:43:55 +0700

    Built GeneralHeader.

--- src/pages/general/GeneralHeader.tsx
+++ src/pages/general/GeneralHeader.tsx
@@ -32,7 +32,9 @@ export default function GeneralHeader() {
 
     // Element:
     return (
-        <div className="flex">
+        <div
+            className="flex items-center justify-items-center h-24 border border-solid border-black"
+        >
             <img
                 alt="User Avatar"
                 src={
@@ -40,12 +42,20 @@ export default function GeneralHeader() {
                     ? `${API_URL}${user.avatar}`
                     : `${API_URL}/assets/avatar/default.png`
                 }
-                className="w-16 h-16 inline-block mr-3"
+                className="w-16 h-16 inline-block mr-3 rounded-full ml-3 cursor-pointer"
             />
 
-            <p className="inline-block font-bold text-2xl align-middle">
+            <p
+                className="inline-block font-bold text-2xl text-left flex-1"
+            >
                 { user?.fullName }
             </p>
+
+            <img
+                alt="Cart Button"
+                src="/shopping-cart.png"
+                className="w-14 h-14 mr-3 cursor-pointer"
+            />
         </div>
     )
 }
