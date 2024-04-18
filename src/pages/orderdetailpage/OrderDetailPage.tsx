922fc7733676072ecda7b57d51580d864629e261
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-18 22:55:46 +0700

    Added OrderDetailPage and building it.

---
+++ src/pages/orderdetailpage/OrderDetailPage.tsx
@@ -0,0 +1,15 @@
+export default function OrderDetailPage() {
+    // Design:
+    return (
+        <div
+            className="flex flex-col justify-start w-1/2 mt-8 border border-black border-solid"
+        >
+            {/* ID */}
+            <p className="font-bold text-lg bg-blue-600">
+                Mã đơn hàng: 170171701717017
+            </p>
+
+            {/*  */}
+        </div>
+    );
+}
