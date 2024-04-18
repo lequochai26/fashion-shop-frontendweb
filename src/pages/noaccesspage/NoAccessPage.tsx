bfbe69635e38dd71ea773138d162703304cabd27
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-18 11:41:19 +0700

    Added NoAccessPage.

---
+++ src/pages/noaccesspage/NoAccessPage.tsx
@@ -0,0 +1,8 @@
+export default function NoAccessPage() {
+    // Design:
+    return (
+        <div className="w-full h-fit font-bold text-3xl text-center">
+            Bạn không thể truy cập vào trang này!
+        </div>
+    );
+}
