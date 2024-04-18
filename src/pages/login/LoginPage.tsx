079cc9b36c825e7cc1d9f18476e9e57483f3d299
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-18 15:32:07 +0700

    Add checkLogged vào LoginPage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Controller from '../../controllers/Controller';
 import LoginPageController, { LoginParam } from '../../controllers/general/LoginPageController';
 import { redirect } from '../../utils/Redirector';
+import CheckLoggedInController, { CheckLoggedInparam } from '../../controllers/general/CheckLoggedInController';
+import NoAccessPage from '../noaccesspage/NoAccessPage';
 
 
 
@@ -10,9 +12,11 @@ export default function LoginPage(){
    //state
    const [email,setEmail] = useState<string>("");
    const [password,setPassword] = useState<string>("");
+   const [loggedIn,setLoggedIn] = useState<boolean>(false);
 
    //controller
    const loginPageController : Controller<LoginParam> = new LoginPageController();
+   const checkLoggedInController : Controller<CheckLoggedInparam> = new CheckLoggedInController();
 
    //event handler
    async function onField(event:any) {
@@ -44,35 +48,58 @@ export default function LoginPage(){
         });
         
     }
+    //innit
+    function init(){
+        checkLoggedInController.execute({
+            onSuccess: function (loggedIn:boolean) {
+                setLoggedIn(loggedIn);
+                
+            },
+            onError : function(error:any){
+                console.error(error);
+            }
+        });
+    }
+    //check login
+    useEffect( init,[]);
   
   
    return(
-       <div className="w-650 h-350 text-center  mt-20 ml-64">
-       <img
-           alt="Fashion Shop's Logo"
-           src="/logo.png"
-           className="w-48 h-48 mb-5 ml-6 mr-auto "
-       />
-       <div className=''>
-       {/* input */}
-       <input
-           className='shadow appearance-none border rounded w-200 h-30 py-2 px-3 text-gray-700 mb-4 mx-auto mr-52'
-           type="email" name='email' onChange={onField}
-       /><br/>
-       <input
-           className="shadow appearance-none border rounded w-200 h-30 py-2 px-3 text-gray-700 mb-4 mx-auto mr-52"
-           type="password"
-           placeholder="Password"
-           required onChange={onField}
-       /><br/>
-       {/* button */}
-       <button className="w-52 h-9 mb-4 rounded-md bg-gray-200 hover:text-blue-500 mx-auto mr-52" onClick={handlerLogin} >
-           Đăng nhập
-       </button><br/>
-       <Link to={''} className="text-black-500 mx-auto mr-52">Chưa có tài khoản ? Đăng ký ngay !</Link>
-       </div>
-           
-   </div>
+    <div>
+        {
+            loggedIn ? (
+                <NoAccessPage/>
+            ): (
+
+                <div className="w-650 h-350 text-center  mt-20 ml-64">
+                    <img
+                        alt="Fashion Shop's Logo"
+                        src="/logo.png"
+                        className="w-48 h-48 mb-5 ml-6 mr-auto "
+                    />
+                    <div className=''>
+                            {/* input */}
+                            <input
+                                className='shadow appearance-none border rounded w-200 h-30 py-2 px-3 text-gray-700 mb-4 mx-auto mr-52'
+                                type="email" name='email' onChange={onField}
+                            /><br/>
+                            <input
+                                className="shadow appearance-none border rounded w-200 h-30 py-2 px-3 text-gray-700 mb-4 mx-auto mr-52"
+                                type="password"
+                                placeholder="Password"
+                                required onChange={onField}
+                            /><br/>
+                            {/* button */}
+                            <button className="w-52 h-9 mb-4 rounded-md bg-gray-200 hover:text-blue-500 mx-auto mr-52" onClick={handlerLogin} >
+                                Đăng nhập
+                            </button><br/>
+                            <Link to={''} className="text-black-500 mx-auto mr-52">Chưa có tài khoản ? Đăng ký ngay !</Link>
+                    </div>
+                
+                </div>
+            )
+        }
+    </div>
    
    
 
