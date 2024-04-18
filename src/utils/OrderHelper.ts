96d3507364f0df7896f0c1e1b760ba229438341d
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-18 21:29:51 +0700

    Finished OrderHelper.

--- src/utils/OrderHelper.ts
+++ src/utils/OrderHelper.ts
@@ -1,9 +1,10 @@
-export function getOrderStatusTitle(status: string) {
+export function getOrderStatusTitle(status: string): string {
     switch (status) {
         case 'APPROVEMENT_AWAITING': return "Đang chờ xác nhận";
         case 'DELIVERING': return "Đang giao";
         case 'SUCCESS': return "Đã hoàn tất";
         case 'PAYMENT_AWAITING': return "Đang chờ thanh toán";
         case 'CANCELLED': return "Đã hủy";
+        default: return "Không rõ";
     }
 }
