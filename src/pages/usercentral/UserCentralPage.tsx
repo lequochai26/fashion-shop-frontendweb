fd8d727b44cdbe407a321959c9f27bc38940d357
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-18 07:08:07 +0700

    Load avatar thành công, còn 2 sự kiện

--- src/pages/usercentral/UserCentralPage.tsx
+++ src/pages/usercentral/UserCentralPage.tsx
@@ -1,24 +1,54 @@
+import User from "../../entities/User";
+import { redirect } from "react-router-dom";
+import { API_URL, makeAPIUrl } from "../../utils/APIFetcher";
+import { useState } from "react";
+
 export default function UserCentralPage() {
+    //State
+    const [user, setUser] = useState<User>();
+    //Event Handler
+    async function onUpdatePersonalInfoClick() {
+        redirect("/updatepersonalinfo");
+    }
+
+    async function onOrderOrderedClickPage() {
+        redirect("/orderedorders")
+    }
+
+    async function onLogoutClick() {
+
+    }
+    //Element 
     return (
 
-        <div className="w-screen w-fit">
+        <div className="w-full h-fit flex flex-wrap justify-center">
 
             <div className="border  rounded-md p-4 space-y-4">
+
+
                 {/* Avatar User */}
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center cursor-pointer">
                     <img
+
                         alt="Avatar User"
-                        src="https://cdn-icons-png.flaticon.com/128/1144/1144760.png"
+                        src={
+                            user
+                                ? `${API_URL}${user.avatar}`
+                                : `${API_URL}/assets/avatar/default.png`
+                        }
                         className="w-16 h-16"
                     />
                 </div>
 
                 {/* User Name */}
-                <p className="text-center">Lê Quốc Hải</p>
+                <p className="text-center">Khách hàng: { user?.fullName }</p>
 
                 {/* Cập nhật thông tin cá nhân */}
-                <div className="flex items-center justify-center space-x-2">
+                <div
+                    onClick={onUpdatePersonalInfoClick}
+                    className="flex items-center justify-center space-x-2 cursor-pointer">
                     <img
+                       
                         className="w-8 h-8"
                         src="https://cdn-icons-png.flaticon.com/128/503/503849.png"
                         alt="Setting icon"
