6a8272ff0242aa6e0ac5933c3c48ea71b793a345
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-19 00:00:14 +0700

    Added formatMetadata method for OrderHelper util module.

--- src/utils/OrderHelper.ts
+++ src/utils/OrderHelper.ts
@@ -8,3 +8,13 @@ export function getOrderStatusTitle(status: string): string {
         default: return "Không rõ";
     }
 }
+
+export function formatMetadata(metadata: any): string {
+    let result: string = "";
+
+    for (const key of Object.keys(metadata)) {
+        result += `${result && ', '}${key}: ${metadata[key]}`;
+    }
+
+    return result;
+}
