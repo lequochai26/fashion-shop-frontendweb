0bf00577bfd0c0ae7b43c6e27ca3100f72b2cccc
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-20 19:07:26 +0700

    Make use of LoadingPage at HomePage.

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -6,10 +6,11 @@ import { makeAPIUrl } from "../../utils/APIFetcher";
 import LoadItemsByKeywordController, { LoadItemsByKeywordParam } from "../../controllers/homepage/LoadItemsByKeywordController";
 import AddToCartController, { AddToCartParam } from "../../controllers/homepage/AddToCartController";
 import { redirect } from "../../utils/Redirector";
+import LoadingPage from "../loadingpage/LoadingPage";
 
 export default function HomePage() {
     // States:
-    const [ items, setItems ] = useState<Item[]>([]);
+    const [ items, setItems ] = useState<Item[] | undefined>(undefined);
     const [ keyword, setKeyword ] = useState<string>("");
 
     // Controllers:
@@ -81,72 +82,76 @@ export default function HomePage() {
 
     // Component return:
     return (
-        <div className="w-full h-fit">
-            {/* Search-bar */}
-            <div className="w-full h-16 flex items-center justify-start border border-black border-solid">
-                {/* Keyword input field */}
-                <input type="text" placeholder="Từ khóa tìm kiếm" className="border border-black border-solid rounded-md p-2 pl-4 w-1/2 ml-3" value={keyword} onChange={onKeywordChange} />
+        !items
+        ? <LoadingPage />
+        : (
+            <div className="w-full h-fit">
+                {/* Search-bar */}
+                <div className="w-full h-16 flex items-center justify-start border border-black border-solid">
+                    {/* Keyword input field */}
+                    <input type="text" placeholder="Từ khóa tìm kiếm" className="border border-black border-solid rounded-md p-2 pl-4 w-1/2 ml-3" value={keyword} onChange={onKeywordChange} />
 
-                {/* Search button */}
-                <button
-                    className="bg-white p-2 pl-3 pr-3 border border-black border-solid rounded-md ml-3"
-                    onClick={onSearchButtonClick}
-                >
-                    Tìm kiếm
-                </button>
+                    {/* Search button */}
+                    <button
+                        className="bg-white p-2 pl-3 pr-3 border border-black border-solid rounded-md ml-3"
+                        onClick={onSearchButtonClick}
+                    >
+                        Tìm kiếm
+                    </button>
 
-                {/* Reload button */}
-                <button
-                    className="bg-white p-2 pl-3 pr-3 border border-black border-solid rounded-md ml-3"
-                    onClick={onReloadButtonClick}
-                >
-                    Tải lại
-                </button>
-            </div>
+                    {/* Reload button */}
+                    <button
+                        className="bg-white p-2 pl-3 pr-3 border border-black border-solid rounded-md ml-3"
+                        onClick={onReloadButtonClick}
+                    >
+                        Tải lại
+                    </button>
+                </div>
 
-            {/* Items list */}
-            <div className="w-full h-fit flex flex-wrap justify-center">
-                {
-                    items.map(
-                        function (item: Item) {
-                            return (
-                                <div key={item.id} className="w-fit h-fit border border-black border-solid flex flex-col justify-center items-center m-3 p-3">
-                                    {/* Avatar */}
-                                    <div>
-                                        <img
-                                            alt="Item's Avatar"
-                                            src={makeAPIUrl(item.avatar)}
-                                            className="w-40 h-40 mb-3"
-                                        />
+                {/* Items list */}
+                <div className="w-full h-fit flex flex-wrap justify-center">
+                    {
+                        items.map(
+                            function (item: Item) {
+                                return (
+                                    <div key={item.id} className="w-fit h-fit border border-black border-solid flex flex-col justify-center items-center m-3 p-3">
+                                        {/* Avatar */}
+                                        <div>
+                                            <img
+                                                alt="Item's Avatar"
+                                                src={makeAPIUrl(item.avatar)}
+                                                className="w-40 h-40 mb-3"
+                                            />
+                                        </div>
+
+                                        {/* Name */}
+                                        <p
+                                            className="w-11/12 h-fit font-bold text-xl mb-3 cursor-pointer"
+                                            onClick={() => onItemNameClick(item)}
+                                        >
+                                            {item.name}
+                                        </p>
+
+                                        {/* Price */}
+                                        <p className="w-11/12 h-fit mb-3">
+                                            Giá: {item.price}
+                                        </p>
+
+                                        {/* Add to cart button */}
+                                        <button
+                                            className="border border-black border-solid rounded-md p-2"
+                                            onClick={() => onAddToCartButtonClick(item)}
+                                        >
+                                            Thêm vào giỏ hàng
+                                        </button>
                                     </div>
-
-                                    {/* Name */}
-                                    <p
-                                        className="w-11/12 h-fit font-bold text-xl mb-3 cursor-pointer"
-                                        onClick={() => onItemNameClick(item)}
-                                    >
-                                        {item.name}
-                                    </p>
-
-                                    {/* Price */}
-                                    <p className="w-11/12 h-fit mb-3">
-                                        Giá: {item.price}
-                                    </p>
-
-                                    {/* Add to cart button */}
-                                    <button
-                                        className="border border-black border-solid rounded-md p-2"
-                                        onClick={() => onAddToCartButtonClick(item)}
-                                    >
-                                        Thêm vào giỏ hàng
-                                    </button>
-                                </div>
-                            );
-                        }
-                    )
-                }
+                                );
+                            }
+                        )
+                    }
+                </div>
             </div>
-        </div>
+        )
     );
 }
 
