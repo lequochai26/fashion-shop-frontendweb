20e09b77bdddd0fccfa0ab92e156586a55c03cf0
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-20 18:58:28 +0700

    Modified NoAccessPage.

--- src/pages/noaccesspage/NoAccessPage.tsx
+++ src/pages/noaccesspage/NoAccessPage.tsx
@@ -1,8 +1,12 @@
 export default function NoAccessPage() {
     // Design:
     return (
-        <div className="w-full h-fit font-bold text-3xl text-center">
-            Không thể truy cập vào trang này!
+        <div className="w-full h-full flex flex-col justify-center">
+            <p
+                className="font-bold text-3xl text-center"
+            >
+                Không thể truy cập vào trang này!
+            </p>
         </div>
     );
 }
