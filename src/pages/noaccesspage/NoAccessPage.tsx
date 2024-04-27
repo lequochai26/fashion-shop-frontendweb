016d8c3c66d9958f1a9b379a0e1fca003d4dd488
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-28 03:13:34 +0700

    Added back button for NoAccessPage.

--- src/pages/noaccesspage/NoAccessPage.tsx
+++ src/pages/noaccesspage/NoAccessPage.tsx
@@ -1,12 +1,28 @@
+import { redirect } from "../../utils/Redirector";
+
+
 export default function NoAccessPage() {
+    // Event handlers:
+    async function onBackButtonClick() {
+        redirect("/");
+    }
+
     // Design:
     return (
         <div className="w-full h-full flex flex-col justify-center">
             <p
-                className="font-bold text-3xl text-center"
+                className="font-bold text-3xl text-center mb-5"
             >
                 Không thể truy cập vào trang này!
             </p>
+
+            <div
+                className="flex flex-row justify-center"
+            >
+                <button className="border border-black border-solid rounded-md py-2 px-5" onClick={onBackButtonClick}>
+                    Quay về trang chủ
+                </button>
+            </div>
         </div>
     );
 }
