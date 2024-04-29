74eba0241e933b50bd331508e00607f027c0b057
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-29 12:01:04 +0700

    Added model classes for Cart.

---
+++ src/entities/cartitem/model/Cart.ts
@@ -0,0 +1,35 @@
+import CurrencyHelper from "../../../utils/CurrencyHelper";
+import CartItem from "./CartItem";
+
+export default class Cart {
+    // Fields:
+    private _items: CartItem[];
+
+    // Constructors:
+    public constructor(items?: CartItem[] | undefined) {
+        this._items = items || [];
+    }
+
+    // Methods:
+    public calcTotalPrice(): number {
+        let totalPrice: number = 0;
+
+        for (const item of this._items) {
+            totalPrice += item.calcTotalPrice();
+        }
+
+        return totalPrice;
+    }
+
+    public getTotalPriceVND(): string {
+        return CurrencyHelper.formatVND(this.calcTotalPrice());
+    }
+
+    // Getters / setters:
+    public get items(): CartItem[] {
+        return this._items;
+    }
+    public set items(value: CartItem[]) {
+        this._items = value;
+    }
+}
---
+++ src/entities/cartitem/model/CartItem.ts
@@ -0,0 +1,52 @@
+import Item from "../../Item/model/Item";
+
+export default class CartItem {
+    // Fields:
+    private _item?: Item | undefined;
+    private _amount?: number | undefined;
+    private _metadata?: any;
+
+    // Constructors:
+    public constructor(
+        item?: Item | undefined,
+        amount?: number | undefined,
+        metadata?: any | undefined
+    ) {
+        this._item = item;
+        this._amount = amount;
+        this._metadata = metadata;
+    }
+
+    // Methods:
+    public calcTotalPrice(): number {
+        let totalPrice: number = 0;
+
+        if (this._item && this._amount) {
+            totalPrice = this._amount * this._item.getPrice(this._metadata);
+        }
+
+        return totalPrice;
+    }
+
+    // Getters / setters:
+    public get metadata(): any {
+        return this._metadata;
+    }
+    public set metadata(value: any) {
+        this._metadata = value;
+    }
+
+    public get amount(): number | undefined {
+        return this._amount;
+    }
+    public set amount(value: number | undefined) {
+        this._amount = value;
+    }
+
+    public get item(): Item | undefined {
+        return this._item;
+    }
+    public set item(value: Item | undefined) {
+        this._item = value;
+    }
+}
