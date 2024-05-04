82fa8203f860ceec3f55771535a3c15aa7560097
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-05 02:04:25 +0700

    Added rules to eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,96 @@ export default [
       "dot-notation": "off",
       "@typescript-eslint/dot-notation": "warn",
       "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": "error"
+      "@typescript-eslint/explicit-member-accessibility": "error",
+
+      "@typescript-eslint/member-ordering": [
+        "warn",
+        {
+          default: {
+            memberTypes: [
+              // Static fields
+              "public-static-field",
+              "protected-static-field",
+              "private-static-field",
+              "static-field",
+
+              // Static methods
+              "public-static-method",
+              "protected-static-method",
+              "private-static-method",
+              "static-method",
+
+              // Field
+              "public-field",
+              "protected-field",
+              "private-field",
+              "field",
+
+              // Constructor
+              "constructor",
+
+              // Methods
+              "public-method",
+              "protected-method",
+              "private-method",
+              "method",
+
+              // Getters / setters
+              "get",
+              "set"
+            ]
+          }
+        }
+      ],
+
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: 'variableLike',
+          format: [ 'camelCase' ]
+        },
+
+        {
+          selector: 'property',
+          format: [ 'camelCase' ],
+          leadingUnderscore: 'allow'
+        },
+
+        {
+          selector: 'property',
+          modifiers: [ 'static' ],
+          format: [ 'camelCase', 'UPPER_CASE' ]
+        },
+
+        {
+          selector: 'property',
+          modifiers: [ 'static', 'public' ],
+          format: [ 'UPPER_CASE' ]
+        },
+
+        {
+          selector: 'method',
+          format: [ 'camelCase' ]
+        },
+
+        {
+          selector: 'classicAccessor',
+          format: [ 'camelCase' ]
+        },
+
+        {
+          selector: 'typeLike',
+          format: [ 'PascalCase' ]
+        },
+
+        {
+          selector: 'enumMember',
+          format: [ 'UPPER_CASE' ]
+        }
+      ],
+
+      "@typescript-eslint/no-array-delete": "error",
+      "@typescript-eslint/no-base-to-string": "warn"
     }
   }
 ];
